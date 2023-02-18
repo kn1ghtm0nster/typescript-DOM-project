bc944f161bafbe1e0fe0ca6d3299b3125e406544
Diego Quintanilla <diego_macbook@Diegos-MBP.home>
2023-02-18 16:53:57 -0600

    add more testing code for DOM project

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,11 @@
 "use strict";
+// simple logging function
 const logMessage = (message) => {
     console.log(message);
 };
-logMessage("logging function works!");
+const input = document.getElementById("todo");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+    alert(input.value);
+    input.value = "";
+});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+// simple logging function
 const logMessage = (message: string): void => {
     console.log(message);
 }
+const input = document.getElementById("todo")! as HTMLInputElement;
+const btn = document.getElementById("btn")!;
 
-logMessage("logging function works!")
+btn.addEventListener("click", function(){
+    alert(input.value);
+    input.value = "";
+})
+
+
